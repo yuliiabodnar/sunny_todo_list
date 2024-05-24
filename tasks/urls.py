d2b956584e4feb6e2b90b2f2ca93f7710a865cea
YuliiaBodnar <bodnar.yuliia@gmail.com>
2024-05-24 14:27:11 +0200

    create tasks base template

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('', views.list, name='list')
+    path('', views.task_list, name='task_list'),
+    path('task/add/', views.task_add, name='task_add'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import Task, Location
 
 
-# Create your views here.
-def list(request):
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_add(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
